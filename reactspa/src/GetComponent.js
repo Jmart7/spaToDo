8452350da22eed2b1c750386e83b77c2525a5c09
Jmart7 <nacho9martinez@hotmail.com>
2022-01-26 14:57:46 -0300

    Finished Add and Delete buttons

--- reactspa/src/GetComponent.js
+++ reactspa/src/GetComponent.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect} from 'react' ;
+import React, {useState, useEffect, useRef} from 'react' ;
 import './GetComponent.css'
 
 const url = 'http://localhost:3010/todo'
 
 const UseEffectGetComponent = () => {
     const [todo, setTodos] = useState ([])
+    const [title, setTitle] = useState ([])
 
     const getTodos = async () => {
         const response = await fetch(url, {
@@ -18,17 +19,37 @@ const UseEffectGetComponent = () => {
         // console.log(todos);
     }
 
-    const addTodos = async (titleOfTodo) => {
-        // const requestOptions = {
-        //     method: 'POST',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify({ title: titleOfTodo })
-        // };
-        // fetch(url, requestOptions)
-        //     .then(response => response.json())
-        //     .then(data => this.setState({ postId: data.id }));
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
     }
 
+    const addTodos = async (titleOfTodo) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: titleOfTodo })
+        };
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ postId: data.id }));
+    }
+
+    const deleteTodos = (urlForDelete) => {
+        fetch(urlForDelete, {method : 'DELETE'})
+            .then(() => this.setState({status: 'Deleted'}))
+    }
+
+    const editTodos = (urlToEdit) => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: 'edita2' })
+        };
+        fetch(urlToEdit, requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ postId: data.id }));
+    }
     useEffect(() => {
         getTodos();
     }, []);
@@ -37,16 +58,22 @@ const UseEffectGetComponent = () => {
         <>
             <h3>To-Do List</h3>
             <div className='addTodo'>
-                <input
-                type="text"
-                id="new-todo-input"
-                className="input"
-                name="text"
-                autoComplete="off"
-                />
-                <button type="submit" className="addButton">{addTodos}
-                Add
-                </button>
+                <article>
+                    <form className='form' onSubmit={(e) => handleSubmit(e)}>
+                        <div className='formBlankSpace'>
+                            <label htmlFor='to-do'></label>
+                            <input type="text" 
+                            id='title' 
+                            name='title' 
+                            value={title} 
+                            onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                    </form>
+                    <button type="push" className="addButton" onClick={() => {addTodos(title); return getTodos()}}>
+                    Add
+                    </button>
+                </article>
             </div>
             <div className='todoGet'>
                 <ul className='todos'>
@@ -54,9 +81,14 @@ const UseEffectGetComponent = () => {
                     const { id, title } = elem;
                     return (
                         <><label key={id} className='form-control'>
-                            <input type="checkbox" name="checkbox" />
-                            {title}
-                            <a href= {'http://localhost:3010/todo' + id} className='deleteButton'>Delete</a>
+                            <input type="checkbox" name="checkbox" className='inputCheckbox' />
+                        </label>
+                        <label>
+                            <a className='form-controlTitle'>{title}</a>
+                            <button onClick={() => {editTodos(url + '/' + id); return getTodos()}} className='editButton'>Edit</button>
+                        </label>
+                        <label className='form-controlDelete'>
+                            <a onClick={() => {deleteTodos(url + '/' + id); return getTodos()}} className='deleteButton'>Delete</a>
                         </label></>
                     );
                     })}
